cbc21140218f54b0bde8ccb3fddf759d573785c9
Ronald Charles Remington <sha1-e2111856323aad618695db98878ea974335b0417@cern.ch>
2008-08-08 22:19:07 +0000

    Removed module that put particle list for calculation of GenMetNoNuBSM into event. This module is already run in RecoJets sequence.

--- python/GenMETParticles_cff.py
+++ python/GenMETParticles_cff.py
@@ -41,19 +41,5 @@ genCandidatesForMET = cms.EDProducer(
     9900016, 39, 12, 13, 14, 16)  ###These ID's will contribute to MET 
 )
 
-genCandidatesForMETNoNuBSM = cms.EDProducer(
-    "InputGenJetsParticleSelector",
-    src = cms.InputTag("genParticles"),
-    partonicFinalState = cms.bool(False),
-    excludeResonances = cms.bool(True),
-    excludeFromResonancePids = cms.vuint32(12,13,14,16 ),
-    tausAsJets = cms.bool(False),
-    ignoreParticleIDs = cms.vuint32(
-    1000022, 2000012, 2000014,
-    2000016, 1000039, 5000039,
-    4000012, 9900012, 9900014,
-    9900016, 39)
- )
-    
-genMETParticles = cms.Sequence(genCandidatesForMET*genCandidatesForMETNoNuBSM)
+genMETParticles = cms.Sequence(genCandidatesForMET)
 
