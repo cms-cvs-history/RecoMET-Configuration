9915ba2d1e7f020dd717c6cb39e3cd4805771f58
Ronald Charles Remington <sha1-e2111856323aad618695db98878ea974335b0417@cern.ch>
2008-10-29 22:02:42 +0000

    Added particle flow MET (pfMet) to the met reconstruction sequence (from R. Cavanaugh)

--- python/RecoMET_cff.py
+++ python/RecoMET_cff.py
@@ -2,17 +2,21 @@ import FWCore.ParameterSet.Config as cms
 
 # Name:   RecoMET.cff
 # Author: R.Cavanaugh
-# Date:   05.11.2006
-# Notes:  CaloMET.cfi assumes that a product with label "caloTowers" is 
+# Date:   28.10.2008
+# Notes:  CaloMET.cfi assumes that a product with label "towerMaker" is 
+#         already written into the event.
+# Notes:  PFMET.cfi assumes that a product with label "particleFlow" is 
 #         already written into the event.
 from RecoJets.Configuration.CaloTowersES_cfi import *
 from RecoMET.METProducers.CaloTowersOpt_cfi import *
 from RecoMET.METProducers.CaloMET_cfi import *
 from RecoMET.METProducers.HTMET_cfi import *
+from RecoMET.METProducers.PFMET_cfi import *
 #sequence metreco = {met, htMetIC5, htMetMC5}
 metreco = cms.Sequence(
     met+metNoHF+metHO+metNoHFHO+
     calotoweroptmaker+metOpt+metOptNoHF+calotoweroptmakerWithHO+metOptHO+metOptNoHFHO+
-    htMetSC5+htMetSC7+htMetKT4+htMetKT6+htMetIC5
+    htMetSC5+htMetSC7+htMetKT4+htMetKT6+htMetIC5+
+    pfMet
     )
 
