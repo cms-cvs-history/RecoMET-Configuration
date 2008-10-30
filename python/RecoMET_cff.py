ca2944f9b9c95840e166261f068a4a5ff414c242
Ronald Charles Remington <sha1-e2111856323aad618695db98878ea974335b0417@cern.ch>
2008-10-30 15:47:39 +0000

    Removed pfMet from the metreco sequence and put into separate sequence.

--- python/RecoMET_cff.py
+++ python/RecoMET_cff.py
@@ -5,18 +5,14 @@ import FWCore.ParameterSet.Config as cms
 # Date:   28.10.2008
 # Notes:  CaloMET.cfi assumes that a product with label "towerMaker" is 
 #         already written into the event.
-# Notes:  PFMET.cfi assumes that a product with label "particleFlow" is 
-#         already written into the event.
 from RecoJets.Configuration.CaloTowersES_cfi import *
 from RecoMET.METProducers.CaloTowersOpt_cfi import *
 from RecoMET.METProducers.CaloMET_cfi import *
 from RecoMET.METProducers.HTMET_cfi import *
-from RecoMET.METProducers.PFMET_cfi import *
 #sequence metreco = {met, htMetIC5, htMetMC5}
 metreco = cms.Sequence(
     met+metNoHF+metHO+metNoHFHO+
     calotoweroptmaker+metOpt+metOptNoHF+calotoweroptmakerWithHO+metOptHO+metOptNoHFHO+
     htMetSC5+htMetSC7+htMetKT4+htMetKT6+htMetIC5+
-    pfMet
     )
 
