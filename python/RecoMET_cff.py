c112fd1c50da2a8bf9da6506ca3250358eaa03a8
Ronald Charles Remington <sha1-e2111856323aad618695db98878ea974335b0417@cern.ch>
2009-02-24 17:43:28 +0000

    Updated on behalf of F. Golf

--- python/RecoMET_cff.py
+++ python/RecoMET_cff.py
@@ -5,14 +5,17 @@ import FWCore.ParameterSet.Config as cms
 # Date:   28.10.2008
 # Notes:  CaloMET.cfi assumes that a product with label "towerMaker" is 
 #         already written into the event.
+
 from RecoJets.Configuration.CaloTowersES_cfi import *
 from RecoMET.METProducers.CaloTowersOpt_cfi import *
 from RecoMET.METProducers.CaloMET_cfi import *
 from RecoMET.METProducers.HTMET_cfi import *
+from RecoMET.METProducers.TCMET_cfi import *
+
 #sequence metreco = {met, htMetIC5, htMetMC5}
 metreco = cms.Sequence(
     met+metNoHF+metHO+metNoHFHO+
     calotoweroptmaker+metOpt+metOptNoHF+calotoweroptmakerWithHO+metOptHO+metOptNoHFHO+
-    htMetSC5+htMetSC7+htMetKT4+htMetKT6+htMetIC5
+    htMetSC5+htMetSC7+htMetKT4+htMetKT6+htMetIC5+tcMet
     )
 
